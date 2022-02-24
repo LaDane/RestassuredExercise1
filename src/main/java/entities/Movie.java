f19424734eb87303cd47ebfc1dcf7dc6741ae834
Aleksander W <us.aw@hotmail.com>
2022-02-24 14:55:05 +0100

    Movie ID updated

--- src/main/java/entities/Movie.java
+++ src/main/java/entities/Movie.java
@@ -16,6 +16,7 @@ public class Movie implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
+    @Column(name = "id", nullable = false)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
