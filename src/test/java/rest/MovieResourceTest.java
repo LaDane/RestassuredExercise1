2dfb1a7b17f0c2f83990cbab2ca5cca0f1a8ebf5
Aleksander W <us.aw@hotmail.com>
2022-02-24 14:29:13 +0100

    Fixed api name error

--- src/test/java/rest/MovieResourceTest.java
+++ src/test/java/rest/MovieResourceTest.java
@@ -101,7 +101,7 @@ public class MovieResourceTest {
     @Test
     public void testServerIsUp() {
         System.out.println("Testing is server UP");
-        given().when().get("/xxx").then().statusCode(200);
+        given().when().get("/movie").then().statusCode(200);
     }
 
     //This test assumes the database contains two rows
@@ -109,7 +109,7 @@ public class MovieResourceTest {
     public void testDummyMsg() throws Exception {
         given()
                 .contentType("application/json")
-                .get("/xxx/").then()
+                .get("/movie/").then()
                 .assertThat()
                 .statusCode(HttpStatus.OK_200.getStatusCode())
                 .body("msg", equalTo("Hello World"));
@@ -119,7 +119,7 @@ public class MovieResourceTest {
     public void testCount() throws Exception {
         given()
                 .contentType("application/json")
-                .get("/xxx/count").then()
+                .get("/movie/count").then()
                 .assertThat()
                 .statusCode(HttpStatus.OK_200.getStatusCode())
                 .body("count", equalTo(3));
